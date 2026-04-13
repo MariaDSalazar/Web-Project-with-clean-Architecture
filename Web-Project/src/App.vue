8147b9cf06beab8729b64bef6027955fbc612f11
Maria del Carmen Salazar Torres <76508755+MariaDSalazar@users.noreply.github.com>
2026-04-13 16:38:33 -0500

    Avances en el proyecto: configuración inicial de Vue.js con Vite

---
+++ Web-Project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="contenedor">
+    <img src="./assets/logo.svg" alt="Vue logo" width="125" height="125" />
+    <small>
+      <h1>Hola mundo desde Vue. js 3</h1>
+    </small>
+    
+    <h2>Contador: 0</h2>
+    <button>+1</button>
+    <button>-1</button>
+    <button>Reiniciar</button>
+  </div>
+</template>
+
+<style scoped>
+.contenedor {
+  text-align: center;
+  background-color : #ddd;
+}
+small {
+  Color:#666
+}
+h1 {
+  font-weight: bolder;
+}
+h2 {
+  font-size: 2.5 rem;
+}
+button {
+  cursor: pointer;
+  font-size: 1.5rem;
+  margin: 0px 6px;
+
+}
+</style>
